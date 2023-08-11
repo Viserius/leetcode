894e2d0ceea87ee3095c79d3e8da39737f7bb08f
Viserius <info@marksoelman.nl>
2023-08-11 11:28:28 +0200

    Create 6. Zigzag Conversion.java

---
+++ 6. Zigzag Conversion.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String convert(String s, int numRows) {
+        // Initialize rows
+        StringBuilder[] rowTexts = new StringBuilder[numRows];
+        for(int i = 0; i < numRows; i++) {
+            rowTexts[i] = new StringBuilder();
+        }
+
+        // Add every character to the corresponding row
+        for(int i = 0; i < s.length(); i++) {
+            int y = this.transformIdxToRow(i, numRows-1);
+            rowTexts[y].append(s.charAt(i));
+        };
+
+        return String.join("", rowTexts);
+    }
+
+    int transformIdxToRow(int i, int maxRowIdx) {
+        if(maxRowIdx == 0)
+            return 0;
+        if((i / maxRowIdx) % 2 == 0) {
+            // Increasing
+            return i % maxRowIdx;
+        } else {
+            // Decreasing
+            return maxRowIdx - i % maxRowIdx;
+        }
+    }
+}
