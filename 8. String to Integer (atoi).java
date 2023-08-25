ec45394d31e359bc2a3f706f4a4430c58187e3c6
Viserius <info@marksoelman.nl>
2023-08-25 15:39:58 +0200

    Create 8. String to Integer (atoi).java

---
+++ 8. String to Integer (atoi).java
@@ -0,0 +1,42 @@
+class Solution {
+    public int myAtoi(String s) {
+        int i = 0, c, result = 0, sign = 1;
+
+        // Ignore whitespace
+        while(i < s.length()) {
+            c = s.charAt(i);
+            if(c == ' ') {
+                i++;
+            } else {
+                break;
+            }
+        }
+
+        // Check positive or negative
+        if(i < s.length() && s.charAt(i) == '+') {
+            i++;
+        } else if(i < s.length() && s.charAt(i) == '-') {
+            sign = -1;
+            i++;
+        }
+
+        // Increment result
+        while(i < s.length()) {
+            c = s.charAt(i);
+            if(c < '0' || c > '9')
+                break;
+
+            int before = result;
+            result = result * 10 + sign * (c - '0');
+
+            // Check for overflow
+            if((result - sign * (c - '0')) / 10 != before || (result > 0 && sign == -1) || (result < 0 && sign == 1)) {
+                return (sign == 1) ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            }
+
+            i++;
+        }
+
+        return result;
+    }
+}
